1717c8a406f958008e70ac654f3163729844bb28
xet-a <csh4519@naver.com>
2024-05-09 16:01:49 +0900

    Feat: read and write contract

--- near-marketplace-contract/src/lib.rs
+++ near-marketplace-contract/src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// UnorderedMap: storage로 사용할 data structure
+// Borsh~: 직렬화, 역직렬화 format
+// near_bindgen: 함수, 구조체가 외부에서 호출 가능하도록 생성
+// PanicOnDefault: contract가 초기화되기 전에 호출되면 panic이 발생하는 Default 속성 구현
+use near_sdk::collections::UnorderedMap;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{near_bindgen, PanicOnDefault};
+
+// declare the structure and its variable
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct MarketPlace {
+    products: UnorderedMap<String, String>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// implement marketplace methods
+#[near_bindgen]
+impl MarketPlace {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // initialize the contract
+    // create a new instance of `UnorderedMap`
+    #[init]
+    pub fn init() -> Self {
+        Self {
+            products: UnorderedMap::new(b"product".to_vec()),
+        }
+    }
+
+    // add a new product to the `products` map
+    // id: key, product_name: value
+    // * the key for the persistent collection should be as short as possible to reduce storage space
+    // because this key will be repeated for every record in the collection *
+    pub fn set_product(&mut self, id: String, product_name: String) {
+        self.products.insert(&id, &product_name);
+    }
+
+    // retreive a product from the `products` map
+    // return either a product name or `null`
+    // * all read methods should be immutable
+    pub fn get_product(&self, id: &String) -> Option<String> {
+        self.products.get(id)
     }
 }
