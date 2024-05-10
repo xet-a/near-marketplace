f1873fa8747b902469d5ae865d28a83053faca79
xet-a <csh4519@naver.com>
2024-05-10 17:21:04 +0900

    Feat: Create Product Model

--- near-marketplace-contract/src/lib.rs
+++ near-marketplace-contract/src/lib.rs
@@ -1,10 +1,13 @@
+use core::str;
+
 // UnorderedMap: storage로 사용할 data structure
 // Borsh~: 직렬화, 역직렬화 format
-// near_bindgen: 함수, 구조체가 외부에서 호출 가능하도록 생성
+// near_bindgen: 함수, 구조체가 외부에서 호출 가능하도록 생성 즉, 유효한 near contract로 변환
 // PanicOnDefault: contract가 초기화되기 전에 호출되면 panic이 발생하는 Default 속성 구현
 use near_sdk::collections::UnorderedMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, PanicOnDefault};
+use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault};
+use serde::{Deserialize, Serialize};
 
 // declare the structure and its variable
 #[near_bindgen]
@@ -39,5 +42,55 @@ impl MarketPlace {
     // * all read methods should be immutable
     pub fn get_product(&self, id: &String) -> Option<String> {
         self.products.get(id)
+    }
+}
+
+#[near_bindgen]
+#[derive(BorshSerialize, BorshDeserialize, Serialize, PanicOnDefault)]
+pub struct Product {
+    id: String,
+    name: String,
+    description: String,
+    image: String,
+    location: String,
+    price: String, // u128 큰 숫자를 문자열로 변환하여 저장
+    owner: AccountId,
+    sold: u32
+}
+
+// used as an intermediate object, Product 구조체로 매핑
+#[near_bindgen]
+#[derive(Serialize, Deserialize, PanicOnDefault)]
+pub struct Payload {
+    id: String,
+    name: String,
+    description: String,
+    image: String,
+    location: String,
+    price: String
+}
+
+#[near_bindgen]
+impl Product {
+    
+    // map Payload to Product
+    // excepts for two properties: `sold`, `owner`
+    // *near-sdk 5.0.0부터 non-init method에서 Self 참조 불가능
+    pub fn from_payload(payload: Payload) -> Product {
+        Self {
+            id: payload.id,
+            description: payload.description,
+            name: payload.name,
+            location: payload.location,
+            price: payload.price,
+            sold: 0,
+            image: payload.image,
+            owner: env::signer_account_id()
+        }
+    }
+
+    // increment the `sold` value after a product has been sold
+    pub fn increment_sold_amount(&mut self) {
+        self.sold = self.sold + 1;
     }
 }
